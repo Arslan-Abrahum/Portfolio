8c8fbfc081d41089ab62af080ad7d0e999704915
Arslan  Ibrahim <abrahum9328@gmail.com>
2024-05-23 19:06:10 +0500

    Changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const ProjectCard = ({ title, description, link, image, category }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 mx-4 my-6 bg-white hover:shadow-2xl card">
-      <img className="w-full h-full object-cover" src={image} alt={title} />
+      <div>
+        <img className="w-full h-full object-cover" src={image} alt={title} />
+
+      </div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2 card-title">{title}</div>
         <p className="text-base mb-4 card-description">{description}</p>
